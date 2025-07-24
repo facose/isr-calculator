0d0a23ebfe14fda04d1f3cd77be362c5473ca5b9
Fabian Conejo Segura <fabianconejo@Fabians-MacBook-Pro.local>
2025-07-23 19:45:40 -0600

    fix: add test and rakefile to packagin script

--- bin/zip_project.rb
+++ bin/zip_project.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/env ruby
 zip_name = "isr_calculator.zip"
 
-puts "Zipping lib/, bin/, and README.md into #{zip_name}..."
+puts "Zipping lib/, bin/, test/, Rakefile, and README.md into #{zip_name}..."
 
 # Create the zip
-system("zip -r #{zip_name} lib bin README.md")
+system("zip -r #{zip_name} lib bin test Rakefile README.md")
 
 puts "Done!"
